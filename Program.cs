19c63e1c51322911dbb0447c3f2945d10787791b
cang688 <cang688@aucklanduni.ac.nz>
2026-09-13 22:53:15 +1200

    testing sending simple telemetry message to cloud

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text;
+
+DotNetEnv.Env.TraversePath().Load(".env");
+
+string deviceConnectionString = Environment.GetEnvironmentVariable("DEVICE_CONNECTION_STRING");
+
+//Console.WriteLine(deviceConnectionString);
+
+var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
+
+var carMessage = new carMessage()
+{
+    isCharging = "true",
+    battery = "80"
+};
+
+var serializedMessage = JsonConvert.SerializeObject(carMessage);
+
+var iotMessage = new Message(Encoding.UTF8.GetBytes(serializedMessage));
+
+// send message to cloud
+await deviceClient.SendEventAsync(iotMessage);
+
+//Console.ReadLine();
+
+class carMessage
+{
+    public string isCharging { get; set; }
+    public string battery { get; set; }
+}
+
